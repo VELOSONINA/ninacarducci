0ba49dc4b6003ef677856e803db53546e9977386
Nirina VELOSONINA <vnirina081@gmail.com>
2023-08-11 12:24:59 +0200

    code js et css minifier

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,21 +1,2 @@
-$(document).ready(function() {
-    $('.carousel').carousel({
-        interval: false,
-    });
+$(document).ready(function(){$('.carousel').carousel({interval:!1,});$('.gallery').mauGallery({columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:!0,lightboxId:'myAwesomeLightbox',showTags:!0,tagsPosition:'top'});$('.gallery img').attr('loading','lazy')})
 
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
-
-    $('gallery img').attr('loading', 'lazy')
-});
